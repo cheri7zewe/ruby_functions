871ca98e393b1a8f04f5a34f5babee3b41f14405
Cheri <cheri.zewe@gmail.com>
2016-08-08 11:34:55 -0400

    zero and negs

--- multiplication/test_multiplication.rb
+++ multiplication/test_multiplication.rb
@@ -7,4 +7,16 @@ class TestMultiplication < Minitest::Test
 		assert_equal(4, my_multiplication(2, 2))
 	end
 
+	def test_3_multiply_4_equals_12
+		assert_equal(12, my_multiplication(3, 4))
+	end
+
+	def test_0_multiply_5_equals_0
+		assert_equal(0, my_multiplication(0, 5))
+	end
+
+	def test_negative_1_multiply_2_equals_negative_2
+		assert_equal(-2, my_multiplication(-1, 2))
+	end
+
 end
